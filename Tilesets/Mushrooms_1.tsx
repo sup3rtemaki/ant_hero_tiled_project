934b470ccaf86c00c04be46bc6005e1293482af8
Celso Junior <celsofer93@gmail.com>
2026-08-24 11:37:05 -0300

    Hero.h

---
+++ Tilesets/Mushrooms_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Mushrooms_1" tilewidth="32" tileheight="32" tilecount="15" columns="5">
+ <image source="../Texture/mushrooms_1.png" width="179" height="121"/>
+</tileset>
